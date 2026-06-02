dabc0faa2e9c03e8986bda3cf0b90825c404eafe
Gabriel Meneses <gabomendua@gmail.com>
2026-06-02 15:47:06 -0600

    comit

--- Ejercicio4/Ejercicio4/obj/Release/net10.0/Ejercicio4.AssemblyInfo.cs
+++ Ejercicio4/Ejercicio4/obj/Release/net10.0/Ejercicio4.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio4")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee194ec0e4499ef1e0c1304169226d482f86fe81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio4")]
