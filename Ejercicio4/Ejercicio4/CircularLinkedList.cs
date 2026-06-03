2cc9d461b2b7007709f4017c38c511d1d003d6cb
Gabriel Meneses <gabomendua@gmail.com>
2026-06-02 18:11:35 -0600

    Commit para armar
    Marcus, te dejo lo demás, solo falta lo de k con eliminar, solo es de llamar la funcion deleteAt para borrar por posición, lit te deje todo para armar

--- Ejercicio4/Ejercicio4/CircularLinkedList.cs
+++ Ejercicio4/Ejercicio4/CircularLinkedList.cs
@@ -19,7 +19,7 @@ namespace Ejercicio4
         {
             Node aux = new Node(value);
 
-            if (this.head == null)
+            if (head == null)
             {
                 head = tail = aux;
                 tail.Next = head;
@@ -55,7 +55,76 @@ namespace Ejercicio4
                 Add(i);
             }
         }
+        public Node GetAt(int pos)
+        {
+            if (IsEmpty() || pos>=Size)
+            {
+                return null;
+            }
 
+            int i = 0;
+            Node current = head;
+            while(current != null)
+            {
+                if(i == pos)
+                {
+                    return current;
+                }
+                i++;
+                current = current.Next;
+            }
+            return null;
+        }
+        private bool DeleteFirst()
+        {
+            if (head == null)
+            {
+                return false;
+            }
+            Node current = head;
+            head = current.Next;
+            current.Next = null;
+            size--;
+            return true;
+        }
+
+        private bool DeleteLast()
+        {
+            if (head == null) { return false; }
+            if (size == 1) { return DeleteFirst(); }
+
+            Node aux = GetAt(Size - 2);
+            aux.Next = null;
+            tail = aux;
+            size--;
+            return true;
+        }
+
+        private bool DeleteAt(int pos)
+        //vaya marcus te dejo esta webada ya ready para que solo hagas el coso de k y dejar ganador
+        {
+            if (head == null || pos > size || pos<0)
+            {
+                return false;
+            }
+
+            if (pos == size-1) //borrar el ultimo
+            {
+                return DeleteLast();
+            }
+
+            if (pos == 0) //borrar el primero
+            {
+                return DeleteFirst();
+            }
+
+            Node aux = GetAt(pos-1);
+            Node deleted = aux.Next;
+            aux.Next = deleted.Next;
+            deleted = null;
+            size--;
+            return false;
+        }
 
     }
 }
